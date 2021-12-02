0a3479f65d116014618fa57acf3a611d225bd845
Emil Eriksson <asheidan@gmail.com>
2021-12-02 09:47:17 +0100

    Minimize and cleanup solution for day 01 b

--- 01/solve_b.py
+++ 01/solve_b.py
@@ -4,20 +4,14 @@ import sys
 
 
 def main() -> None:
-    numbers = [int(l.strip()) for l in sys.stdin.readlines()]
-    # print(repr(numbers))
+    numbers = [int(line.strip()) for line in sys.stdin.readlines()]
 
-    windows = list(zip(numbers[:-2], numbers[1:-1], numbers[2:]))
-    # print(windows)
+    # When comparing two consecutive sliding windows it is enough to compare
+    # the unique elements between the windows
+    pairs = list(zip(numbers[:-3], numbers[3:]))
+    result = sum(1 for a, b in pairs if a < b)
 
-    # for window in windows:
-    #     print("%-15s | %d" % (window, sum(window)))
-
-    sums = [sum(window) for window in windows]
-    pairs = list(zip(sums[:-1], sums[1:]))
-    result = [1 for a, b in pairs if a < b]
-
-    print(sum(result))
+    print(result)
 
 
 if __name__ == "__main__":
